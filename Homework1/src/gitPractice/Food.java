4a563701587a6cc3b677053f16bc93b4521e5e56
Seema Vora <seema.s.vora@gmail.com>
2021-02-06 19:08:32 -0800

    Creates a JavaDoc for the Classes

--- Homework1/src/gitPractice/Food.java
+++ Homework1/src/gitPractice/Food.java
@@ -3,6 +3,10 @@ package gitPractice;
 public class Food {
 
 	String favoriteFood;
+	/**
+	 * 
+	 * @param favoriteFood: identifies user's favorite food
+	 */
 	
 	public Food (String favoriteFood) {
 		this.favoriteFood = favoriteFood;
