7df21b38e9673272dd590f20a8b4f00d7cafbd0d
Seema Vora <seema.s.vora@gmail.com>
2021-05-05 18:34:46 -0700

    Uncomments Test Cases

--- Homework3/src/Test.java
+++ Homework3/src/Test.java
@@ -1,19 +1,19 @@
-//import static org.junit.Assert.assertTrue;
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//import bargraphMVC.controller.AddNMessage;
-//import bargraphMVC.model.BarModel;
-//
-//
-//class Test {
-//
-//	@org.junit.jupiter.api.Test
-//	public void testSetNum() {
-//		BarModel test = new BarModel();
-//		test.setNum("1", "2", "3");
-//		String num1 = test.getNum();
-//		assertEquals(num1, "1");
-//	}
-//	
-//
-//}
+import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
+
+import bargraphMVC.controller.AddNMessage;
+import bargraphMVC.model.BarModel;
+
+
+class Test {
+
+	@org.junit.jupiter.api.Test
+	public void testSetNum() {
+		BarModel test = new BarModel();
+		test.setNum("1", "2", "3");
+		String num1 = test.getNum();
+		assertEquals(num1, "1");
+	}
+	
+
+}
